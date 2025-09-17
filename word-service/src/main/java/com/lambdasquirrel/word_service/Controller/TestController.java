c847048eb45605a482a61f0c1e0e6cf0f9256e9d
사용자 junyoung011019 <kidjjy5@naver.com>
2025-09-17 23:26:22 +0900

    개발 환경 구축
    - Spring 프로젝트 생성
    - Docker 개발 환경 구축
    - 기본 API 테스트 및 MySQL 연동 확인

---
+++ word-service/src/main/java/com/lambdasquirrel/word_service/Controller/TestController.java
@@ -0,0 +1,15 @@
+package com.lambdasquirrel.word_service.Controller;
+
+import org.apache.coyote.Response;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class TestController {
+
+    @GetMapping("/test")
+    public String test(){
+        return "good";
+    }
+}
---
+++ word-service/src/test/java/com/lambdasquirrel/word_service/WordServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lambdasquirrel.word_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WordServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
